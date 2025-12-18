20dbcb1d83833f23c576ab03c34446f224a701bf
praveenkumar2511 <praveenks2511@gmail.com>
2025-12-18 17:31:37 +0530

    feat:add ci pipeline

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 console.log("hello devops")
+console.log("I'm Laerning CI/CD using github actions")
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Testing............")
+setTimeout(() => {
+     console.log("Waiting 3 seconds....");
+     
+}, 3000);
+console.log("Tesing Finished...");
